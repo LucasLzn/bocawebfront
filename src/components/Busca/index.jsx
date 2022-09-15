e6d7bf62734fea0d98cd4e19879d62046a777da4
Moshe370 <68689466+Moshe370@users.noreply.github.com>
2022-09-15 11:31:12 -0400

    Update index.jsx

--- src/components/Busca/index.jsx
+++ src/components/Busca/index.jsx
@@ -1,16 +1,25 @@
+import SearchIcon from "@mui/icons-material/Search";
+
 export const Busca = ({busca, buscaODA}) => {
     return(
 
         <div className = 'busca'>
 
-          <input
-            name= 'busca'
-            type = 'text'
-            value={busca}
-            placeholder='O que deseja buscar'
-            onChange={buscaODA}
+          <span className="spanBusca">
+            
+            <SearchIcon className="iconBusca" />
 
-          />
+            <input
+              name= 'busca'
+              type = 'text'
+              value={busca}
+              placeholder='O que deseja buscar'
+              onChange={buscaODA}
+
+            />
+          
+          </span>
+
         </div>
     )
     
