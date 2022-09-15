b19865405f5b8b58691e9820cf66dd883550f28c
Moshe370 <68689466+Moshe370@users.noreply.github.com>
2022-09-15 11:32:09 -0400

    Update index.jsx

--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -1,12 +1,20 @@
 export const Lista = ({key,nome,usuario,descricao,data_inclusao, palavras_chave, odas}) => {
     return(
-            <div className='dadosODA' key={key}>
-                <h1> nome: {nome} </h1>
-                <p> usuário: {usuario} </p>
-                <p> descrição: {descricao} </p>
-                <p> data de inclusão: {data_inclusao} </p>
-                <p> palavras chave: {palavras_chave} </p>
-            </div>
+      <div className="boxODAS">
+
+        <div className="blockODAS">
+        
+          <div className='dadosODA' key={key}>
+              <h1> nome: {nome} </h1>
+              <p> usuário: {usuario} </p>
+              <p> descrição: {descricao} </p>
+              <p> data de inclusão: {data_inclusao} </p>
+              <p> palavras chave: {palavras_chave} </p>
+          </div>
+        
+        </div>
+
+      </div>
     )
     
     
