9e981a17d4c763cbf8811a0625c84b27016542a9
Moshe370 <68689466+Moshe370@users.noreply.github.com>
2022-09-19 09:13:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Busca } from './components/Busca';
 import { Lista } from './components/Lista';
 import { Cabecalho } from './components/Cabecalho';
 import { Rodape } from './components/Rodape';
+import DocumentMeta from "react-document-meta";
 
 class App extends Component{
 
@@ -20,7 +21,7 @@ class App extends Component{
 
   carregaODAs(){
     const {busca} = this.state;
-    fetch('http://www.bocaweb.com.br/apibocaweb?nome='+busca)
+    fetch('https://www.bocaweb.com.br/apibocaweb?nome='+busca)
     .then(response => response.json())
     .then(odas => this.setState({odas}))
     console.log({busca})
@@ -32,37 +33,55 @@ class App extends Component{
   }
 
   render(){
+
+    const meta = {
+      title: "BocaWebFront",
+      description: "n/a ideais",
+      meta: {
+        charset: "utf-8",
+        name: {
+          viewport: "width=device-width, scale=1"
+        }
+      }
+    };
+
     const {busca,odas} = this.state;
     return (
-      <section className = "container">
 
-        <Cabecalho />
+      <DocumentMeta {...meta}>
+      
+        <section className = "container">
 
-        <Busca 
-        
-          busca={this.state.busca}
-          buscaODA={this.buscaODA}
+          <Cabecalho />
 
-        />
+          <Busca 
+          
+            busca={this.state.busca}
+            buscaODA={this.buscaODA}
 
-        <div className = 'lista'> 
-            <p className="lengthODAS"> {odas.length} odas </p>
-            {odas.map(oda => (
-              <Lista
-                key = {oda.id}
-                nome = {oda.nome} 
-                usuario = {oda.usuario} 
-                descricao = {oda.descricao} 
-                data_inclusao = {oda.data_inclusao}
-                palavras_chave = {oda.palavras_chave}
-              />
-            ))}
-        </div>
+          />
 
-        <Rodape />      
-        
+          <div className = 'lista'> 
+              <p className="lengthODAS"> {odas.length} odas </p>
+              {odas.map(oda => (
+                <Lista
+                  key = {oda.id}
+                  nome = {oda.nome} 
+                  usuario = {oda.usuario} 
+                  descricao = {oda.descricao} 
+                  data_inclusao = {oda.data_inclusao}
+                  palavras_chave = {oda.palavras_chave}
+                />
+              ))}
+          </div>
 
-      </section>
+          <Rodape />      
+          
+
+        </section>
+
+      </DocumentMeta>
+
     )
   }
 }
